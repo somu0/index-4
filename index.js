1a7a063e4a1f69e2f380a16cfbe66587b9d25747
somu0 <94071371+somu0@users.noreply.github.com>
2021-12-25 17:48:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+switch(char) {
+  case 'a':
+  case 'A':
+    alert("It is a vowel");
+    break;
+  case 'e':
+  case 'E':
+    alert("It is a vowel");
+    break;
+  case 'i':
+  case 'I':
+    alert("It is a vowel 1");
+    break;
+  case 'o':
+  case 'O':
+    alert("It is a vowel 2");
+    break;
+  case 'u':
+  case 'U':
+    alert("It is a vowel");
+    break;
+  default:
+    alert("It is a consonant");
+    break;
+}
+ 
